f7b8268001e06ced0d7760d79e61fc60e2361cab
Teake Nutma <teake.nutma@gmail.com>
2013-07-06 21:29:29 +0200

    * Rewrote buildanddeploy.xml slightly. * Moved build directory of docs back to /build, because the doc link tools otherwise don't work ...

--- PacletInfo.m
+++ PacletInfo.m
@@ -4,7 +4,7 @@
 
 Paclet[
     Name -> "xTras",
-    Version -> "1.1.3.14",
+    Version -> "1.1.3.16",
     MathematicaVersion -> "6+",
     Description -> "Field theory inspired additions to xAct",
     Creator -> "Teake Nutma",
