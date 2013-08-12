44a168f138300e08a7b6484394eb9fa9ceed143f
Teake Nutma <teake.nutma@gmail.com>
2013-08-12 15:59:39 +0200

    * Fixed bug in the changelog formatter.

--- PacletInfo.m
+++ PacletInfo.m
@@ -4,7 +4,7 @@
 
 Paclet[
     Name -> "xTras",
-    Version -> "1.1.3.50",
+    Version -> "1.2.0",
     MathematicaVersion -> "6+",
     Description -> "Field theory inspired additions to xAct",
     Creator -> "Teake Nutma",
