ade74400fd75050b03980da0cc1248ec2dfd93bb
Teake Nutma <teake.nutma@gmail.com>
2013-08-21 13:54:23 +0200

    * Updated installation instructions.

--- PacletInfo.m
+++ PacletInfo.m
@@ -4,7 +4,7 @@
 
 Paclet[
     Name -> "xTras",
-    Version -> "1.2.1",
+    Version -> "1.2.1.1",
     MathematicaVersion -> "6+",
     Description -> "Field theory inspired additions to xAct",
     Creator -> "Teake Nutma",
